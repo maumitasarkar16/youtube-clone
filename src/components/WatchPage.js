2c6fc408920ff44ada64f1021a19f7e2816d00c0
Maumita <maumita.sarkar16@gmail.com>
2024-04-21 02:01:46 +0800

    added channel title, views, likes, video title

--- src/components/WatchPage.js
+++ src/components/WatchPage.js
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import { useDispatch } from 'react-redux';
 import { hideSidePanel } from '../utils/sidePanelSlice';
 import { useSearchParams } from 'react-router-dom';
@@ -7,12 +7,22 @@ import LiveChat from './LiveChat';
 
 const WatchPage = () => {
 
-  const [searchParams] = useSearchParams()
   const dispatch = useDispatch();
+  const [searchParams] = useSearchParams()
+  const [details, setDetails] = useState([])
 
   useEffect(() => {
     dispatch(hideSidePanel())
+    getSingleVideoDetails()
   }, [])
+
+  const getSingleVideoDetails = async () => {
+    const data = await fetch(process.env.REACT_APP_SINGLE_YOUTUBE_VIDEO_API + searchParams.get("v"))
+    const json = await data.json();
+    console.log(json.items[0])
+    setDetails(json.items[0])
+    
+  }
 
   return (
     <div className='flex flex-row'>
@@ -29,6 +39,20 @@ const WatchPage = () => {
             allowFullScreen
           ></iframe>
         </div>
+
+        <div> <h1 className='pl-4 mx-5 my-2 font-bold text-2xl'>{details.snippet.title}</h1> </div>
+        <div className='flex justify-between'>
+          <div>
+            <h3 className='pl-4 mx-5 my-2 font-semibold text-lg '>Channel Name: {details.snippet.channelTitle}</h3>
+            <h3 className='pl-4 m-5 '>Views: {details.statistics.viewCount}</h3>
+          </div>
+
+          <div className='flex '>
+            
+            <h3 className='p-4 m-2 pr-24'>Likes: {details.statistics.likeCount}</h3>
+          </div>
+        </div>
+
         <div><CommentContainer videoId={searchParams.get("v")} /></div>
       </div>
       <LiveChat />
